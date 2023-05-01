741810fb327a9f5505da75e38e13fe6396fc50fc
SAGAR SOLANKI <118007645+Sagarsolanki97@users.noreply.github.com>
2023-05-01 12:18:04 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -3,10 +3,6 @@ var open = document.getElementById("open-modal");
 var form =  document.getElementById("form");
 var closeForm =document.getElementById("closeModal");
 
-// inside- form variables :
-var Name = document.getElementById("name");
-console.log(Name.innerText);
-
 open.addEventListener("click", function() {
     form.style.display = "block";
 });
@@ -14,3 +10,52 @@ closeForm.addEventListener("click",function(){
     form.style.display ="none";
 });
 
+
+// inside- form variables :
+
+var formElem = document.getElementById("form");
+formElem.addEventListener("submit",function(event){
+    event.preventDefault();
+    const Name = document.getElementById("name").value;
+    const priority = document.getElementById("priority").value;
+    const dueDate = document.getElementById("due-Date").value;
+    const Status = document.getElementById("Status").value;
+    console.log(Name);
+    console.log(priority);
+    console.log(dueDate);
+
+    console.log(Status);
+    addTotask(Name , priority, dueDate, Status);
+  
+
+    form.style.display ="none";
+});
+
+function addTotask(Name , priority, dueDate, Status){
+    if(Status =="Notstarted"){
+        var column = document.getElementById("notstart");
+        column.innerHTML += `<li>
+                                <h3>${Name}</h3>
+                                <p>${priority}</p>        
+                                <p>${dueDate}</p>
+                                <p>${Status}</p>
+                                </li>`;   }
+    if(Status =="In-progress"){
+        var column = document.getElementById("in-progress");
+        column.innerHTML += `<li>
+                                <h3>${Name}</h3>
+                                <p>${priority}</p>        
+                                <p>${dueDate}</p>
+                                <p>${Status}</p>
+                                </li>`;
+    }
+    if(Status =="Completed"){
+        var column = document.getElementById("completed");
+        column.innerHTML += `<li>
+                                <h3>${Name}</h3>
+                                <p>${priority}</p>        
+                                <p>${dueDate}</p>
+                                <p>${Status}</p>
+                                </li>`;
+    }
+};
